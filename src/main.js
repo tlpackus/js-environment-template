4b7669f5e6d92564578d2cfc0c65df4fcf5d9351
tlpackus <tlpackus@gmail.com>
2021-03-27 18:53:06 -0700

    add bootstrap

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import './css/styles.css';
 // import Example from './example.js';
 import $ from 'jquery';
-// import 'bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
 $(document).ready(function() {
