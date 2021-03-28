50415c7d5f85ada036d9a73186943d78913f6357
tlpackus <tlpackus@gmail.com>
2021-03-27 18:51:39 -0700

    add jquery

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './css/styles.css';
 // import Example from './example.js';
-// import $ from 'jquery';
+import $ from 'jquery';
 // import 'bootstrap';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
